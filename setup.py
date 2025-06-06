60b9619cd8b7a32c583234e5d24b1e2234501878
Raj <rajsoluky5323@gmail.com>
2025-06-06 06:34:14 +0530

    project structure ready, venv set

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Rajkumar Solanki",
+    author_email="rajsoluky5323@gmail.com",
+    packages = find_packages()
+)
